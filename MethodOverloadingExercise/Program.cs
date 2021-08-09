aff3ffa1561de2bdb8ed00884b5d95a1c28b9fa8
jcrowe91 <jcrowe9119@gmail.com>
2021-08-08 22:00:46 -0500

    Second Commit

--- MethodOverloadingExercise/Program.cs
+++ MethodOverloadingExercise/Program.cs
@@ -8,14 +8,16 @@ namespace MethodOverloadingExercise
         {
             int a = 22;
             int b = 40;
-            //decimal c = .12;
-            //decimal d = .55;
+            decimal c = 6.12m;
+            decimal d = 5.55m;
             int e = 130;
             int f = 900;
 
             var answer1 = Add(a, b);
-            //var answer2 = Add(c, d);
+            var answer2 = Add(c, d);
             var answer3 = Add(e, f, true);
+            Console.WriteLine($"int add: {answer1} decimal add: {answer2}");
+            Console.WriteLine(answer3);
         }
 
         public static int  Add(int x, int y)
